196f27e4b0d4d86fdb72a2f172e95f5f13adfd1d
guoyingshou <guoyingshou@tianji.com>
2013-03-06 21:55:59 +0800

    changed file upload with nio2

--- media-comp/src/main/java/com/tissue/media/web/spring/controllers/ImageController.java
+++ media-comp/src/main/java/com/tissue/media/web/spring/controllers/ImageController.java
@@ -1,9 +1,11 @@
 package com.tissue.media.web.spring.controllers;
 
+import com.tissue.core.social.Account;
 import com.tissue.media.services.ImageService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,35 +28,35 @@ import java.io.File;
 import java.io.IOException;
 import java.security.InvalidParameterException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class ImageController {
+
+    private static Logger logger = LoggerFactory.getLogger(ImageController.class);
 
     @Autowired
     private ImageService imageService;
 
-    @RequestMapping(value="/images", method=POST)
-    public String addImage(@RequestParam("CKEditorFuncNum") String num, @RequestParam("upload") MultipartFile file, Map model) throws IOException {
+    @RequestMapping(value="/images/_create", method=POST)
+    public String addImage(@RequestParam("CKEditorFuncNum") String num, @RequestParam("upload") MultipartFile file, Map model, @ModelAttribute("viewerAccount") Account viewerAccount) {
 
-        /**
-        System.out.println("uploaded: " + file);
-        String imgName = UUID.randomUUID().toString() + ".png";
-        
-        File dest = new File("../images/" + imgName);
-        file.transferTo(dest);
-        */
+        List<String> images = Arrays.asList("image/jpeg", "image/png", "image/gif");
+        String type = file.getContentType();
+        if(!images.contains(type)) {
+            throw new IllegalArgumentException(type + " is not supported");
+        }
 
         try {
-            String imageUrl = imageService.saveFile(file);
+            String imageUrl = imageService.saveFile(file, viewerAccount.getUsername());
             model.put("imageUrl", imageUrl);
+            model.put("num", num);
         }
         catch(Exception exc) {
-            exc.printStackTrace();
-            throw new InvalidParameterException("invalid op");
+            throw new IllegalArgumentException(exc.getMessage());
         }
 
-        model.put("num", num);
-        //model.put("imageUrl", "http://www.tissue.com/images/" + imgName);
-        
         return "uploadImageCallback"; 
     }
 
--- media-service/src/main/java/com/tissue/media/services/ImageService.java
+++ media-service/src/main/java/com/tissue/media/services/ImageService.java
@@ -1,63 +1,66 @@
 package com.tissue.media.services;
 
 import org.springframework.web.multipart.MultipartFile;
-/**
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
-*/
+
 import org.im4java.core.ConvertCmd;
 import org.im4java.core.IMOperation;
 import org.im4java.core.Info;
 
 import java.util.UUID;
 import java.io.File;
-import java.security.InvalidParameterException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
 
-//@Component
 public class ImageService {
 
     private String webRoot = "http://www.tissue.com/images/";
-    private String rootPath = "/home/guoyingshou/working/github/images/";
+    private String dataRoot = "/home/guoyingshou/working/github/images/";
 
-    public void setRootPath(String rootPath) {
-       this.rootPath = rootPath;
+    public void setWebRoot(String rootPath) {
+       this.webRoot = webRoot;
     }
 
-    public String saveFile(MultipartFile file) {
-        
-        try {
-        File tmpDir = new File(rootPath + "tmp");
-        File dest = File.createTempFile("image-", "", tmpDir);
-        String tmpName = dest.getAbsolutePath();
-        System.out.println(tmpName);
+    public void setDataRoot(String dataRoot) {
+       this.dataRoot = dataRoot;
+    }
 
-        file.transferTo(dest);
+    public String saveFile(MultipartFile multipartFile, String username) throws Exception {
 
-        Info info = new Info(tmpName);
+        String imageName = UUID.randomUUID().toString() + ".png";
+
+        Path dir = Paths.get(dataRoot);
+
+        Path userRoot = dir.resolve(username);
+        Path rawDataDir = userRoot.resolve("raw");
+
+        if(Files.notExists(rawDataDir)) {
+            Files.createDirectories(rawDataDir);
+        }
+
+        Path rawFile = rawDataDir.resolve(multipartFile.getOriginalFilename());
+        multipartFile.transferTo(rawFile.toFile());
 
         ConvertCmd cmd = new ConvertCmd();
+
         IMOperation op = new IMOperation();
-        op.addImage(tmpName);
+        op.addImage(rawFile.toString());
 
-        int w = info.getImageWidth();
-        if(w > 650) {
-            op.resize(640);
-            //op.format("png");
+        Info info = new Info(rawFile.toString());
+        if(info.getImageWidth() > 600) {
+            op.resize(600);
         }
-        
-        String imgName = UUID.randomUUID().toString() + ".png";
-        String name = rootPath + imgName;
-        op.addImage(name);
 
-        cmd.run(op);
-        return webRoot + imgName;
+        Path imageFile = userRoot.resolve(imageName);
+        op.addImage(imageFile.toString());
 
-        }
-        catch(Exception exc) {
-            throw new InvalidParameterException(exc.getMessage());
-        }
-        finally {
+        IMOperation pngTargetConverter = new IMOperation();
+        pngTargetConverter.format("PNG");
+        pngTargetConverter.addOperation(op);
 
-        }
+        cmd.run(pngTargetConverter);
+
+        return webRoot + "/" + username + "/" + imageName;
     }
 }
